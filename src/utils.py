13e289e821ef7bb654ca8ab46e6c5ac41d377519
2910harshit <harshitkumar2910@gmail.com>
2024-03-27 02:08:17 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.exception import customexception
 
 def save_object(file_path,obj):
@@ -19,14 +19,22 @@ def save_object(file_path,obj):
     except Exception as e:
         raise customexception(e,sys)
     
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
 
+            para=param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
+
+            #model.fit(x_train,y_train)
             y_train_pred=model.predict(x_train)
             y_test_pred=model.predict(x_test)
 
@@ -34,7 +42,7 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
             train_model_score=r2_score(y_train,y_train_pred)
             test_model_score=r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]]=test_model_score
-        print("Report", report)
+        #print("Report", report)
             
 
         return report
